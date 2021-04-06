52c3545fc758ed09cb23bc7e05cf6b0622219ca9
Daan <438919@student.fontys.nl>
2021-04-06 15:03:20 +0200

    Added 2 classes

---
+++ src/main/java/com/example/demo/Backlayer/Player.java
@@ -0,0 +1,17 @@
+package com.example.demo.Backlayer;
+
+import java.util.List;
+
+public class Player {
+
+    public String username;
+    private String password;
+    public Integer elo;
+    public List<String> PlayedGames;
+
+    public Player(String Username, String password, Integer Elo) {
+        username = UserName;
+        password = Password;
+        elo = Elo;
+    }
+}
---
+++ src/main/java/com/example/demo/Backlayer/PlayerLog.java
@@ -0,0 +1,9 @@
+package com.example.demo.Backlayer;
+
+public class PlayerLog {
+    public Player player;
+
+    public void Register() {
+        Player p = new Player();
+    }
+}
