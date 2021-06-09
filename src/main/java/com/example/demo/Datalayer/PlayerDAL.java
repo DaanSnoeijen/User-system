bf9c50535932a681f48a0a324c044222594b5587
DaanSnoeijen <438919@student.fontys.nl>
2021-06-09 19:24:29 +0200

    Update PlayerDAL.java

--- src/main/java/com/example/demo/Datalayer/PlayerDAL.java
+++ src/main/java/com/example/demo/Datalayer/PlayerDAL.java
@@ -1,6 +1,8 @@
 package com.example.demo.Datalayer;
 
 import com.example.demo.Entity.PlayerEntity;
+import com.example.demo.Entity.Player_Tournament;
+import com.example.demo.Entity.TournamentEntity;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
@@ -11,6 +13,9 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class PlayerDAL implements IPlayer {
@@ -79,5 +84,107 @@ public class PlayerDAL implements IPlayer {
             em.close();
             transaction = null;
         }
+    }
+
+    public void UpdatePlayer(int ID, PlayerDTO player){
+        factory = Persistence.createEntityManagerFactory("user-system");
+        em = factory.createEntityManager();
+        transaction = em.getTransaction();
+
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaUpdate<PlayerEntity> update = builder.createCriteriaUpdate(PlayerEntity.class);
+        Root<PlayerEntity> root = update.from(PlayerEntity.class);
+
+        if (player.getUsername() != "" && player.getUsername() != null){
+            update.set("username", player.getUsername());
+        }
+        if (player.getPassword() != "" && player.getPassword() != null){
+            update.set("password", player.getPassword());
+        }
+        update.set("elo", player.getElo());
+
+        update.where(builder.equal(root.get("Id"), ID));
+
+        try{
+            transaction.begin();
+            em.createQuery(update).executeUpdate();
+            transaction.commit();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            transaction.rollback();
+        }
+        finally {
+            factory.close();
+            em.close();
+            transaction = null;
+        }
+    }
+
+    public void DeletePlayer(int ID){
+        factory = Persistence.createEntityManagerFactory("user-system");
+        em = factory.createEntityManager();
+        transaction = em.getTransaction();
+
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaDelete<PlayerEntity> delete = builder.createCriteriaDelete(PlayerEntity.class);
+        Root<PlayerEntity> root = delete.from(PlayerEntity.class);
+
+        delete.where(root.get("Id").in(ID));
+
+        try{
+            transaction.begin();
+            em.createQuery(delete).executeUpdate();
+            transaction.commit();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            transaction.rollback();
+        }
+        finally {
+            factory.close();
+            em.close();
+            transaction = null;
+        }
+    }
+
+    public PlayerDTO GetPlayerTournament(int ID){
+        factory = Persistence.createEntityManagerFactory("user-system");
+        em = factory.createEntityManager();
+        transaction = em.getTransaction();
+
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<PlayerEntity> query = builder.createQuery(PlayerEntity.class);
+        Root<PlayerEntity> root = query.from(PlayerEntity.class);
+        Root<Player_Tournament> PTroot = query.from(Player_Tournament.class);
+        Root<TournamentEntity> Troot = query.from(TournamentEntity.class);
+        Join<PlayerEntity, Player_Tournament> betweenJoin = root.join(String.valueOf(Player_Tournament.class), JoinType.INNER);
+        Join<PlayerEntity, TournamentEntity> endJoin = root.join(String.valueOf(TournamentEntity.class), JoinType.INNER);
+
+        List<Predicate> predicates = new ArrayList<>();
+        predicates.add(builder.equal(root.get("Id"), ID));
+        predicates.add(builder.equal(PTroot.get("Id"), ID));
+
+
+//        query.multiselect(,
+//                )
+
+        try{
+            transaction.begin();
+            PlayerEntity entity = em.createQuery(query).getSingleResult();
+            playerDTO = new PlayerDTO(entity);
+            transaction.commit();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            transaction.rollback();
+        }
+        finally {
+            factory.close();
+            em.close();
+            transaction = null;
+        }
+
+        return playerDTO;
     }
 }
